20b683f62b860af39f36a88df670461b42adada6
Damilola <dhurcasdamilola15@gmail.com>
2022-03-08 12:07:05 +0100

    sorted rental performance

--- models/dvdrental_models/rental_performance.sql
+++ models/dvdrental_models/rental_performance.sql
@@ -22,48 +22,30 @@ all_col as(
 select title, 
 	rental_duration, 
 	extract('day' from return_date-rental_date) as rent_diff
-	-- case when rent_diff < rental_duration then 'film returned early' 
-	-- when rent_diff = rental_duration then 'film returned on time' 
-	-- else 'film returned late'
-	-- end as return_t,
-	-- count(*) as return_time
 	--extract('day' from rental_date) as rental_day
 	from film
 	join inventory using (film_id)
 	join rental using(inventory_id)
 	),
 
-select 
-	case when rent_diff < rental_duration then 'film returned early' 
-	when rent_diff = rental_duration then 'film returned on time' 
+return as(
+	select 
+	--title, 
+	--rental_duration, 
+	---(return_day - rental_day) as rent_diff ,
+	case when rent_diff < rental_duration then 'film returned early'
+	when rent_diff = rental_duration then 'film returned on time'
 	else 'film returned late'
-	end as return_t,
-	count(*) as return_time
-
--- return_duration as(
--- 	select 
--- 	title, 
--- 	rental_duration, 
--- 	--(return_date - rental_day) as rent_diff ,
--- 	case when rent_diff < rental_duration then 'film returned early' 
--- 	when rent_diff = rental_duration then 'film returned on time' 
--- 	else 'film returned late'
--- 	end as return_t,
--- 	count(*) as return_time)
-
-final as(
-	select
-	title,
-	rental_duration
-	--return_time
+	end as return
 	from all_col
-	--join return_duration 
-	group by 1,2
-	order by 2 desc
-)
+),
 
-select * from final
---return_time, 
---	count(return_time) as number from final
-	--group by 1
---order by 2 desc
+return_time as(
+	select
+	return, 
+	count(return) as number from return
+	group by 1
+order by 2 desc
+	)
+	
+select * from return_time
